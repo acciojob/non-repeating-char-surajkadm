0a7e82b205c68f8c04cfdb3bb12ef0fd4872d1dd
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-17 12:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+var str=prompt("Enter a string");
+let splitedArr=str.split("");
+console.log(splitedArr);
+var newEle="";
+var i=0;
+while(i<splitedArr.length ) {
+	if(splitedArr[i]==splitedArr[i+1]){
+		i=i+2;
+	}
+	if(splitedArr[i]!=splitedArr[i+1]){
+		newEle=splitedArr[i];
+	}
+	i++;
+	
+	}
+alert(newEle);
