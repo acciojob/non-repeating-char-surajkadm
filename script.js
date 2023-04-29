48cd9f6453a96a3ce354067ef37ca9a64011c324
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-29 14:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //your JS code here. If required.
-var str=prompt("Enter a string");
-let splitedArr=str.split("");
-console.log(splitedArr);
-var newEle="";
-var i=0;
-for (let i = 0; i < splitedArr.length; i++) {
-	for (let j = 0; j < splitedArr.length; j++){
-		if(splitedArr[i]==splitedArr[j]&&i!=j){
-			console.log(splitedArr[j]);
-			break;
-		}else{
-			newEle=splitedArr[j]
-		}
-		}
-	}
-alert(newEle);
+
+function firstNonRepeatedChar(str) {
+  // Loop through each character in the string
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    
+    // If this is the first and last occurrence of the character in the string, return it
+    if (str.indexOf(char) === i && str.lastIndexOf(char) === i) {
+      return char;
+    }
+  }
+  
+  // If no non-repeated character was found, return null or undefined
+  return null;
+}
+
+
+var input=prompt("Enter a string");
+const output = firstNonRepeatedChar(input);
+alert(output);
